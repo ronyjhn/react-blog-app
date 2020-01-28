bfe76c3b001cb6c56e9327c9fab2729c9511b4fa
ronyjhn <ronyjhn@gmail.com>
2020-01-28 23:25:36 +0530

    1 file modified, 1 added

--- components/App.js
+++ components/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import api from '../api';
 
 class App extends React.Component{
 
+  state={ users:[],isLoading:true};
+
+  async componentDidMount(){
+
+    const response = await api.get('users');
+    const users = await response.data;
+    this.setState({users: users, isLoading:false});
+    
+  }
+
+  renderUsers(){
+    return this.state.users.map( user=>{
+      return <div  key={user.id} className='item'>{user.name}</div>
+    });
+
+  }
+
   render(){
-    return <div>App</div>;
+    if(this.state.isLoading){
+      return <div>Loading.....</div>
+    }
+    return <div className='list'>{this.renderUsers()}</div>;
   }
 
 }
